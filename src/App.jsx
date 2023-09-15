23fb9440891d2ad40884a210d0917326430ec726
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-09-15 18:28:23 +0200

    Navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <main className='relative'>
-      Nav
+      <Nav />
       <section className='xl:padding-1 wide:padding-r padding-b'>
-        Hero
+        <Hero />
       </section>
       <section className='padding'>
-        PopularProducts
+        <PopularProducts />
       </section>
       <section className='padding'>
-        SuperQuality
+        <SuperQuality />
       </section>
       <section className='padding-x py-10'>
-        Services
+        <Services />
       </section>
       <section className='padding'>
-        SpecialOffers
+        <SpecialOffer />
       </section>
       <section className='padding'>
-        CustomerReviews
+        <CustomerReviews />
       </section>
       <section className='padding-x sm:py-32 py-16 w-full'>
-        Subscribe
+        <Subscribe />
       </section>
       <section className='bg-black padding-x padding-t pb-8'>
-        Footer
+        <Footer />
       </section>
     </main>
   )
